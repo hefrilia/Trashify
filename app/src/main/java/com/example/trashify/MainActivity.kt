c5bccc244934f373f14ea0a282d4a7a5794d4e5d
hefrilia <hefrilianurafifah@gmail.com>
2023-12-04 17:35:52 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/trashify/MainActivity.kt
+++ app/src/main/java/com/example/trashify/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.trashify
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
