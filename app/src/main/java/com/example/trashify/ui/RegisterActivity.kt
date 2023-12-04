9a73ceaba31f007b7ef5f99ec3f9e0065e11c23c
Hefrilia Nur Afifah <94454760+hefrilia@users.noreply.github.com>
2023-12-04 16:57:11 +0700

    setup project

---
+++ app/src/main/java/com/example/trashify/ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.trashify.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.trashify.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
